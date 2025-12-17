41b10ed416889189f8779481279307796ce7fd16
kindgood612-boop <kindgood612@gmail.com>
2025-12-18 00:12:41 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+# ========== BOT ==========
+BOT_TOKEN = "8517776533:AAEgwzqn4EtkIZtAhpi33LU9EMqDk3KHpCc"
+
+# ========== DEVELOPERS ==========
+MAIN_DEVELOPER_ID = 8313557781  # حط الـ ID هنا
+SECONDARY_DEVELOPERS = []
+
+# ========== DATABASE ==========
+DATABASE_PATH = "database/bot.db"
+
+# ========== MODES ==========
+PAID_MODE = False
+FORCED_SUB_CHANNEL = None
